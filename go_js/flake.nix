f579a032f2d1f25d50a6de37c73f278a9cd705bc
Chance Kupersmith <kupersmith3.6@gmail.com>
2024-03-08 14:29:50 -0800

    made init client script executable

--- go_js/flake.nix
+++ go_js/flake.nix
@@ -35,7 +35,8 @@
               node2nix nodejs pnpm yarn
             ];
             shellHook = ''
-              chmod +x init_client_app.sh && ./init_client_app.sh
+            chmod +x init_client_app.sh
+            ./init_client_app.sh
             '';
           };
         });
