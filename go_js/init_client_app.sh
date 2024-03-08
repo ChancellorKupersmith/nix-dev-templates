9d483f5ae33176f33f958dc5c00eee93834f3551
Chance Kupersmith <kupersmith3.6@gmail.com>
2024-03-08 15:01:16 -0800

    added init dist/index.html to resolve not pulling from github bug

--- go_js/init_client_app.sh
+++ go_js/init_client_app.sh
@@ -11,6 +11,25 @@ if [ -d "./client" ]; then
   # init client side env
   cd client || exit 1
 
+  # create index.html in dist (bug: not pulling from git)
+  if [! -d "./dist" ]; then
+    mkdir dist
+    echo '<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Dude suh</title>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script type="module" src="src/bundle.js"></script>
+    </body>
+    </html>'' > dist/index.html
+  else
+    echo "dist folder was pulled from github!"
+  fi
+
   pnpm i -D esbuild react react-dom || catch_err "Failed to install client dependencies"
 
   cd "$START_DIR" && exit 0
