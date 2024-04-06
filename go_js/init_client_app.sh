e27e5e667950f52c7ef9d8fa8d337190359806dd
ChancellorKupersmith <kupersmith3.6@gmail.com>
2024-04-05 20:01:26 -0700

    changed init client script to use vite instead of esbuild

--- go_js/init_client_app.sh
+++ go_js/init_client_app.sh
@@ -7,32 +7,13 @@ catch_err() {
   cd "$START_DIR" && exit 1
 }
 
-if [ -d "./client" ]; then
-  # init client side env
-  cd client || exit 1
-
-  # create index.html in dist (bug: not pulling from git)
-  if [ ! -d "./dist" ]; then
-    mkdir dist
-    echo '<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Dude suh</title>
-    </head>
-    <body>
-      <div id="root"></div>
-      <script type="module" src="src/bundle.js"></script>
-    </body>
-    </html>' > dist/index.html
-  else
-    echo "dist folder was pulled from github! now safe to delete this conditional"
-  fi
-
-  pnpm i -D esbuild react react-dom || catch_err "Failed to install client dependencies"
-
-  cd "$START_DIR" && exit 0
-else
-  catch_err "client dir not found"
+if [ ! -d "./client" ]; then
+  # init react client side env
+  pnpm i -D create vite client --template react
+  # init dependency install
+  cd client || catch_err "Failed to cd into client directory"
+  pnpm install || catch_err "Failed to install client dependencies"
+  pnpm run build || catch_err "Failed to build client dist package"
 fi
+
+cd "$START_DIR" && exit 0
