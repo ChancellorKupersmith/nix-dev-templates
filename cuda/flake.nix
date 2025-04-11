56444ae235a0b54933a1a7f51cc9984fd0b38e2e
ChancellorKupersmith <kupersmith3.6@gmail.com>
2025-04-11 08:43:13 -0700

    removed darwin from cuda templates supported systems, will cross that bridge if I ever get a mac with nvidia drivers ;)

--- cuda/flake.nix
+++ cuda/flake.nix
@@ -11,7 +11,7 @@
 
     outputs = { self, nixpkgs, nixpkgs-unstable }:
     let
-        supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+        supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
         forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
             pkgs = import nixpkgs { inherit system; };
             unstable-pkgs = import nixpkgs-unstable { 
