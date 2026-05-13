05c9ddffc9ec9b9fd2cc667877c49f1d44deee7e
ChancellorKupersmith <kupersmith3.6@gmail.com>
2026-05-13 13:16:52 -0700

    added python tdd-aider workflow

--- flake.nix
+++ flake.nix
@@ -3,10 +3,12 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       overlays = [
-        (final: prev:
+        (
+          final: prev:
           let
             getSystem = "SYSTEM=$(nix eval --impure --raw --expr 'builtins.currentSystem')";
             forEachDir = exec: ''
@@ -54,202 +56,228 @@
                 nix flake update
               '';
             };
-          })
+          }
+        )
       ];
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit overlays system; };
+          }
+        );
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ build check format update ];
-        };
-      });
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              build
+              check
+              format
+              update
+            ];
+          };
+        }
+      );
 
-      packages = forEachSupportedSystem ({ pkgs }: rec {
-        default = dvt;
-        inherit (pkgs) dvt;
-      });
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        rec {
+          default = dvt;
+          inherit (pkgs) dvt;
+        }
+      );
     }
 
     //
 
-    {
-      templates = rec {
-        clojure = {
-          path = ./clojure;
-          description = "Clojure development environment";
-        };
+      {
+        templates = rec {
+          aider = {
+            path = ./aider;
+            description = "Human-Led Architectural TDD for Aider";
+          };
+          clojure = {
+            path = ./clojure;
+            description = "Clojure development environment";
+          };
 
-        csharp = {
-          path = ./csharp;
-          description = "C# development environment";
-        };
+          csharp = {
+            path = ./csharp;
+            description = "C# development environment";
+          };
 
-        cuda = {
-          path = ./cuda;
-          description = "CUDA development environment";
-        };
+          cuda = {
+            path = ./cuda;
+            description = "CUDA development environment";
+          };
 
-        cue = {
-          path = ./cue;
-          description = "Cue development environment";
-        };
+          cue = {
+            path = ./cue;
+            description = "Cue development environment";
+          };
 
-        dhall = {
-          path = ./dhall;
-          description = "Dhall development environment";
-        };
+          dhall = {
+            path = ./dhall;
+            description = "Dhall development environment";
+          };
 
-        elixir = {
-          path = ./elixir;
-          description = "Elixir development environment";
-        };
+          elixir = {
+            path = ./elixir;
+            description = "Elixir development environment";
+          };
 
-        elm = {
-          path = ./elm;
-          description = "Elm development environment";
-        };
+          elm = {
+            path = ./elm;
+            description = "Elm development environment";
+          };
 
-        empty = {
-          path = ./empty;
-          description = "Empty dev template that you can customize at will";
-        };
+          empty = {
+            path = ./empty;
+            description = "Empty dev template that you can customize at will";
+          };
 
-        gleam = {
-          path = ./gleam;
-          description = "Gleam development environment";
-        };
+          gleam = {
+            path = ./gleam;
+            description = "Gleam development environment";
+          };
 
-        go = {
-          path = ./go;
-          description = "Go (Golang) development environment";
-        };
+          go = {
+            path = ./go;
+            description = "Go (Golang) development environment";
+          };
 
-        go_js = {
-          path = ./go_js;
-          description = "Go (Golang) backend, JS frontend development environment";
-        };
+          go_js = {
+            path = ./go_js;
+            description = "Go (Golang) backend, JS frontend development environment";
+          };
 
-        hashi = {
-          path = ./hashi;
-          description = "HashiCorp DevOps tools development environment";
-        };
+          hashi = {
+            path = ./hashi;
+            description = "HashiCorp DevOps tools development environment";
+          };
 
-        haskell = {
-          path = ./haskell;
-          description = "Haskell development environment";
-        };
+          haskell = {
+            path = ./haskell;
+            description = "Haskell development environment";
+          };
 
-        java = {
-          path = ./java;
-          description = "Java development environment";
-        };
+          java = {
+            path = ./java;
+            description = "Java development environment";
+          };
 
-        kotlin = {
-          path = ./kotlin;
-          description = "Kotlin development environment";
-        };
+          kotlin = {
+            path = ./kotlin;
+            description = "Kotlin development environment";
+          };
 
-        latex = {
-          path = ./latex;
-          description = "LaTeX development environment";
-        };
+          latex = {
+            path = ./latex;
+            description = "LaTeX development environment";
+          };
 
-        nickel = {
-          path = ./nickel;
-          description = "Nickel development environment";
-        };
+          nickel = {
+            path = ./nickel;
+            description = "Nickel development environment";
+          };
 
-        nim = {
-          path = ./nim;
-          description = "Nim development environment";
-        };
+          nim = {
+            path = ./nim;
+            description = "Nim development environment";
+          };
 
-        nix = {
-          path = ./nix;
-          description = "Nix development environment";
-        };
+          nix = {
+            path = ./nix;
+            description = "Nix development environment";
+          };
 
-        node = {
-          path = ./node;
-          description = "Node.js development environment";
-        };
+          node = {
+            path = ./node;
+            description = "Node.js development environment";
+          };
 
-        opa = {
-          path = ./opa;
-          description = "Open Policy Agent development environment";
-        };
+          opa = {
+            path = ./opa;
+            description = "Open Policy Agent development environment";
+          };
 
-        php = {
-          path = ./php;
-          description = "PHP development environment";
-        };
+          php = {
+            path = ./php;
+            description = "PHP development environment";
+          };
 
-        protobuf = {
-          path = ./protobuf;
-          description = "Protobuf development environment";
-        };
+          protobuf = {
+            path = ./protobuf;
+            description = "Protobuf development environment";
+          };
 
-        pulumi = {
-          path = ./pulumi;
-          description = "Pulumi development environment";
-        };
+          pulumi = {
+            path = ./pulumi;
+            description = "Pulumi development environment";
+          };
 
-        purescript = {
-          path = ./purescript;
-          description = "Purescript development environment";
-        };
+          purescript = {
+            path = ./purescript;
+            description = "Purescript development environment";
+          };
 
-        python = {
-          path = ./python;
-          description = "Python development environment";
-        };
+          python = {
+            path = ./python;
+            description = "Python development environment";
+          };
 
-        ros = {
-          path = ./ros;
-          description = "ROS2 development enviornment";
-        };
+          ros = {
+            path = ./ros;
+            description = "ROS2 development enviornment";
+          };
 
-        ruby = {
-          path = ./ruby;
-          description = "Ruby development environment";
-        };
+          ruby = {
+            path = ./ruby;
+            description = "Ruby development environment";
+          };
 
-        rust = {
-          path = ./rust;
-          description = "Rust development environment";
-        };
+          rust = {
+            path = ./rust;
+            description = "Rust development environment";
+          };
 
-        rust-toolchain = {
-          path = ./rust-toolchain;
-          description = "Rust development environment with Rust version defined by a rust-toolchain.toml file";
-        };
+          rust-toolchain = {
+            path = ./rust-toolchain;
+            description = "Rust development environment with Rust version defined by a rust-toolchain.toml file";
+          };
 
-        scala = {
-          path = ./scala;
-          description = "Scala development environment";
-        };
+          scala = {
+            path = ./scala;
+            description = "Scala development environment";
+          };
 
-        shell = {
-          path = ./shell;
-          description = "Shell script development environment";
-        };
+          shell = {
+            path = ./shell;
+            description = "Shell script development environment";
+          };
 
-        vlang = {
-          path = ./vlang;
-          description = "Vlang developent environment";
-        };
+          vlang = {
+            path = ./vlang;
+            description = "Vlang developent environment";
+          };
 
-        zig = {
-          path = ./zig;
-          description = "Zig development environment";
-        };
+          zig = {
+            path = ./zig;
+            description = "Zig development environment";
+          };
 
-        # Aliases
-        rt = rust-toolchain;
+          # Aliases
+          rt = rust-toolchain;
+        };
       };
-    };
 }
