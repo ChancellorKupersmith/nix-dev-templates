e9ec43e8764ea1cad62c9e90850826b46a54e4ee
ChancellorKupersmith <kupersmith3.6@gmail.com>
2026-05-26 17:43:33 -0700

    added flutter flake

--- flake.nix
+++ flake.nix
@@ -146,6 +146,12 @@
             description = "Empty dev template that you can customize at will";
           };
 
+          flutter = {
+            path = ./flutter;
+            description = "Flutter development environment";
+
+          };
+
           gleam = {
             path = ./gleam;
             description = "Gleam development environment";
